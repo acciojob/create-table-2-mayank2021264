86b9fab6d1293f4940b657311575fcb881d9dedc
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-25 21:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function createTable() {
     //Write your code here
-  
+    let rows = prompt("Input number of rows");
+	let cols = prompt("Input number of columns");
+
+	if(rows<=0 || cols <=0) return "";
+	
+	for(let i=0;i<rows;i++){
+		let rowHTML = `<tr>`;
+		console.log("row created");
+		for(let j=0;j<cols;j++){
+			rowHTML += `<td>Row-${i} Column-${j}</td>`;
+		}
+		rowHTML += `</tr>`;
+		table.innerHTML += rowHTML;
+	}
 }
+
+let table = document.getElementById('myTable');
+table.innerHTML = "";
